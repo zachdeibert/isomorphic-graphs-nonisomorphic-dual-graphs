1a9ea1ca6a157b0dde55c9700184ec2b25c46ee2
Zach Deibert <zachdeibert@gmail.com>
2018-08-28 10:19:06 -0500

    Added list impl

---
+++ logic/list.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <string.h>
+#include <list.h>
+
+int list_append(list_node_t *list, void *value) {
+    int i;
+    for (i = 0; list->next; ++i) {
+        list = list->next;
+    }
+    list_node_t *node = (list_node_t *) malloc(sizeof(list_node_t));
+    node->previous = list;
+    node->next = NULL;
+    node->value = value;
+    list->next = node;
+    return i;
+}
+
+void list_insert(list_node_t *list, void *value) {
+    list_node_t *node = (list_node_t *) malloc(sizeof(list_node_t));
+    node->previous = list;
+    node->next = list->next;
+    node->value = value;
+    list->next = node;
+}
+
+void list_insert2(list_node_t *list, void *value, int i) {
+    for (; i > 0; --i) {
+        list = list->next;
+    }
+    list_insert(list, value);
+}
+
+int list_length(list_node_t *list) {
+    int i;
+    for (i = 1; list->next; ++i) {
+        list = list->next;
+    }
+    return i;
+}
+
+int list_total_length(list_node_t *list) {
+    int i;
+    for (i = list_length(list); list->next; ++i) {
+        list = list->next;
+    }
+    return i;
+}
+
+void *list_remove(list_node_t *list) {
+    list->previous->next = list->next;
+    list->next->previous = list->previous;
+    void *val = list->value;
+    free(list);
+    return val;
+}
+
+void *list_remove2(list_node_t *list, int i) {
+    for (; i > 0; --i) {
+        list = list->next;
+    }
+    return list_remove(list);
+}
+
+void list_clear(list_node_t *list) {
+    if (list->next) {
+        do {
+            list = list->next;
+            free(list->previous);
+        } while (list->next);
+        free(list);
+    }
+}
+
+void list_clear_and_free(list_node_t *list) {
+    if (list->next) {
+        do {
+            free(list->value);
+            list = list->next;
+            free(list->previous);
+        } while (list->next);
+        free(list);
+        free(list->value);
+    }
+}
+
+void list_to_array(list_node_t *list, void *arr, int size) {
+    while (list) {
+        memcpy(arr, list->value, size);
+        arr += size;
+        list = list->next;
+    }
+}
---
+++ logic/list.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+struct list_node;
+typedef struct list_node list_node_t;
+
+struct list_node {
+    list_node_t *previous;
+    list_node_t *next;
+    void *value;
+};
+
+int list_append(list_node_t *list, void *value);
+void list_insert(list_node_t *list, void *value);
+void list_insert2(list_node_t *list, void *value, int i);
+int list_length(list_node_t *list);
+int list_total_length(list_node_t *list);
+void *list_remove(list_node_t *list);
+void *list_remove2(list_node_t *list, int i);
+void list_clear(list_node_t *list);
+void list_clear_and_free(list_node_t *list);
+void list_to_array(list_node_t *list, void *arr, int size);
+
+#endif
